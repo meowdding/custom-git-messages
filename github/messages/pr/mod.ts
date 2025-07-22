e2204e765c0b2624fe89d9e42ea2b49a8713a25b
Mona <59416038+meowora@users.noreply.github.com>
2025-07-22 09:14:34 +0200

    feat: add draft and undraft messages

--- github/messages/pr/mod.ts
+++ github/messages/pr/mod.ts
@@ -3,11 +3,15 @@ import { Closed } from "./closed.ts";
 import { Reopened } from "./reopened.ts";
 import { HandlerList, NoMessage } from "../../../main.ts";
 import { GithubMessage } from "../../mod.ts";
+import { Drafted } from "./converted_to_draft.ts";
+import { Undrafted } from "./ready_for_review.ts";
 
 const actions: HandlerList<GithubMessage | undefined> = {
     "opened": Opened,
     "closed": Closed,
-    "reopened": Reopened
+    "reopened": Reopened,
+    "converted_to_draft": Drafted,
+    "ready_for_review": Undrafted,
 };
 
 //deno-lint-ignore no-explicit-any
