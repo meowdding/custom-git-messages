31d06a8c6e5c8f2c5c2c894156f7b9181fc953ad
Mona <59416038+meowora@users.noreply.github.com>
2025-11-02 22:12:15 +0100

    chore: change mortem color

--- github/projects.ts
+++ github/projects.ts
@@ -25,7 +25,7 @@ export const colors: { [project: string]: number } = {
     "sillygames": 1,
     "cosmetics-backend": 1,
     "catharsis": 0xD21F64,
-    "mortem": 0x2B2B2B
+    "mortem": 0xf08080
 }
 
 export const abbreviations: { [project: string]: string }  = {
