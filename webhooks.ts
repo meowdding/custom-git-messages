fbf8b15035e629f2c99a7e87e661d0de2c5a09e2
Mona <git@mona.cat>
2026-06-27 07:23:15 +0200

    fix: import

--- webhooks.ts
+++ webhooks.ts
@@ -1,5 +1,5 @@
 import { post, WebhookMessage } from "https://deno.land/x/dishooks@v1.1.0/mod.ts";
-import { ServiceResponse } from "./main";
+import { ServiceResponse } from "./main.ts";
 
 const debugUrl = Deno.env.get("debug_webhook_url"); // used for redelivered events
 const prodUrl = Deno.env.get("webhook_url");
